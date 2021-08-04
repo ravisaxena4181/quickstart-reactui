ce5dbe57fbdd68d4de79752f75390bc3578d26ad
Ravi Saxena <43087386+ravisaxena4181@users.noreply.github.com>
2021-08-04 16:36:55 +0530

    added webpack

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -25,8 +25,8 @@ export default class SideBar extends Component {
                     <a href="friends" className="list-group-item list-group-item-action"><i className="material-icons">insert_emoticon</i>Friends <small className="text-mute">(250)</small></a>
                     <a href="subscribed" className="list-group-item list-group-item-action"><i className="material-icons">library_books</i>Subscribed</a>
                     <a href="notifications" className="list-group-item list-group-item-action"><i className="material-icons">notifications</i>Notification <span className="badge badge-dark text-white">2</span></a>
-                    <a href="setting" className="list-group-item list-group-item-action"><i className="material-icons">account_circle</i>Setting</a>
-                    <a href="elements" className="list-group-item list-group-item-action"><i className="material-icons">account_circle</i>Pages & Elements</a>  
+                    {/* <a href="setting" className="list-group-item list-group-item-action"><i className="material-icons">account_circle</i>Setting</a>
+                    <a href="elements" className="list-group-item list-group-item-action"><i className="material-icons">account_circle</i>Pages & Elements</a>   */}
                 </div>
             </div>
         )
